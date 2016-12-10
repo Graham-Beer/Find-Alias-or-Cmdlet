d9b100941a7c24e95be58c9bf500641dfa5347dc
Graham Beer <graham.beer@outlook.com>
2016-12-10 10:53:50 +0000

    Update Get-Altname.ps1

--- Get-Altname.ps1
+++ Get-Altname.ps1
@@ -1,1 +1,10 @@
-
+Function Get-AltName ($CmdletToAlias,$AliasToCmdlet) {
+# Alternate name: Alias or cmdlet         
+    if ($CmdletToAlias) {
+        $Alias = Get-Alias | where {$_.ResolvedCommandName -eq "$CmdletToAlias"} | select @{Name="Alias Name";Expression={$_.name}}
+            if ($Alias -eq $Null) { "No Alias Found" }Else{ Return $Alias }     
+    }else{       
+        $Cmdlet = get-alias | where {$_.Name -eq "$AliasToCmdlet"} | select @{Name="Cmdlet Name";Expression={$_.ResolvedCommand}}
+            if ($Cmdlet -eq $Null) { "No Alias Found" }Else{ Return $Cmdlet }      
+    }
+}
